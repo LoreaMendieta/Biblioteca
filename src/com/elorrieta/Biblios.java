de955aa556851884b87e2846b447d7b8228694b4
Lorea <inflmendieta@elorrieta-errekamari.com>
2019-12-05 17:08:07 +0100

    Menu Principal base hecha, falta rellenar opciones

--- src/com/elorrieta/Biblios.java
+++ src/com/elorrieta/Biblios.java
@@ -1,10 +1,36 @@
 package com.elorrieta;
 
+import java.util.Scanner;
+
 public class Biblios {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Inicio de Proyecto");
+		System.out.println("Por favor, Intorduzca la operacion que quiera realizar:");
+		System.out.println("A: Sacar un libro");
+		System.out.println("B: Devolver un libro");
+		System.out.println("S: Salir");
+		System.out.println("");
+		System.out.println("Seleccione una opcion valida:");
+		
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		char opcion=sc.nextLine().toUpperCase().charAt(0);//Me quedo con la primera letra que haya introducido si mete AF me quedo con la A
+	
+		switch (opcion) {
+		case 'A':
+	
+		break;
+		case 'B':
+			
+			break;
+		case 'S':
+			
+			break;
+		default:
+		break;
+	} 
 	}
 
 }
